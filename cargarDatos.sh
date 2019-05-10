9387359832edc06ea76df2503aecd8bbd1b4f47e
matiasbian <matiasezequielbian@gmail.com>
2019-05-10 18:48:15 -0300

    agregado script que carga datos

---
+++ cargarDatos.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+node main.js addArtist RingoStarr Inglaterra
+
+node main.js addArtist ElvisPresley EstadosUnidos
+
+node main.js addArtist LucianoPereyra Argentina
+
+node main.js addArtist AriannaGrande EstadosUnidos
+
+node main.js addArtist KatyPerry EstadosUnidos
+
+node main.js addArtist LadyGaga EstadosUnidos
+
+node main.js addAlbum 0 SentimentalJourney 1970
+
+node main.js addAlbum 0 Ringo 1973
+
+node main.js addAlbum 0 GoodnightVienna 1974
+
+node main.js addAlbum 1 MoodyBlue 1977
+
+node main.js addAlbum 1 Elvis 1956
+
+node main.js addAlbum 1 BlueHawaii 1961
+
+node main.js addAlbum 2 LaVidaAlViento 2017
+
+node main.js addAlbum 3 YoursTrully 2013
+
+node main.js addAlbum 3 YoursTrully 2013
+
+node main.js addAlbum 3 MYEverything 2014
+
+node main.js addAlbum 3 DangerousWoman 2016
+
+node main.js addAlbum 5 ArtPop 2013
+
+node main.js addAlbum 5 TheFrame 2008
+
+node main.js addAlbum 5 Joanne 2016
+
+node main.js addAlbum 4 Witness 2017
+
+node main.js addAlbum 4 Prism 2013
+
+node main.js addAlbum 4 TeenageDream 2010
+
+node main.js addTrack 8 Ocapella 4 Rock Indie
+
+node main.js addTrack 8 CallMe 3 Rock Country
+
+node main.js addTrack 8 No No Song 2 Rock Industrial
+
+node main.js addTrack 6 SentimentalJourney 4 Rock Pop
+
+node main.js addTrack 6 ByeByeBlackBird 4 Rock Pop
+
+node main.js addTrack 6 Dream 3 Rock Pop
+
+node main.js addTrack 7 Photograph 4 Rock Pop
+
+node main.js addTrack 7 OhMy 4 Rock Pop
+
+node main.js addTrack 7 AllOfMe 3 Rock Pop
+
+node main.js addTrack 9 GoNow 3 Rock Blues
+
+node main.js addTrack 9 ImJustASinger 3 Rock Blues
+
+node main.js addTrack 9 NevercomesTheDay 3 Rock Blues
+
+node main.js createPlaylist SadMusic 15 Blues
+
+node main.js createPlaylist PopMusic 15 Pop
+
+
+
+
+
+
+
+
