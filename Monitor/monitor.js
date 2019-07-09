74e2c14f1613890951351a9852bd87271cb63e52
matiasbian <matiasezequielbian@gmail.com>
2019-07-09 12:09:42 -0300

    Monitor terminado

--- Monitor/monitor.js
+++ Monitor/monitor.js
@@ -2,11 +2,11 @@
 
 const express = require('express')
 const rp = require('request-promise');
-const app = express()
+const app = express();
 
-const apiErrors = require("../ApiErrors")
+const apiErrors = require("../ApiErrors");
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const router = express.Router();
 
@@ -25,54 +25,77 @@ const options = {
 };
 let verificacionActivada = true;
 let lastStatus = "";
-const mensajeDeRestaablecimiento = "[GOOD]El servidor volvió a funcionar a las: ";
-const mensajeDeCayoServer = "[BAD]El servidor dejo de funcionar a las: ";
 
-router.get("/status", (req,res,next) => {
-    getStatus();
+
+router.get("/status", (req,res,next) => { //endpoint para activar o desactivar el monitor
     res.status(200);
     verificacionActivada = !verificacionActivada;
     res.json("Servicio " + ((verificacionActivada) ? "Activado" : "Desactivado"))
 })
 
-const notificarViaSlack = function(status) {
 
-    if (status != lastStatus){
-        lastStatus = status;
-        if (status == "OK"){
-            console.log(mensajeDeRestaablecimiento + new Date());
-        }else{
-            console.log(mensajeDeCayoServer + new Date());
-        }
-        //options.url = unqfyURL + '/artists/0'; // URL del slack
-        //mando consulta A LA API DEL ESLAC
-    }   
-}
 
-const getStatus = function(){
+const getStatus = function(){ //llamada a la api para ver si el server esta ON
     options.url = unqfyURL + '/artists/0';
     return rp.get(options);
 }
 
 
-const checkStatus = function(){
+const checkStatus = function(){ // Si esta activado el monitor, checkea si cambio el estado
     if (verificacionActivada){
         let status = "";
         getStatus()
         .then( resp => {
             console.log("El servidor funciona");
-            status = "OK";
+            status = "ON";
             notificarViaSlack(status);
         })
         .catch( error => {
             console.log("El Servidor No funciona");
-            status = "NO OK";
+            status = "OFF";
             notificarViaSlack(status);
         })
     }
+}
+
+const notificarViaSlack = function(status) { //notifico por slack
+    if (status != lastStatus){
+        lastStatus = status;
+ 
+        const url = 'https://hooks.slack.com/services/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';
+        var payload = {"text": new MonitorMessage(status).Notify()}
+        payload = JSON.stringify(payload);
+
+        rp.post({url: url, body: payload})
+        .then(resp => {
+            console.log("Enviada notificacion al slack");
+        })
+        .catch(error => {
+            console.log(error.message);
+        })      
+    }   
 }
 
 
 app.listen(port);
 console.log('Api Ready! ' + port);
 setInterval(checkStatus, 5000);
+
+class MonitorMessage{
+    constructor(status){
+        this.status = status;
+        this.mensajeDeRestaablecimiento = "[ON]El servidor volvió a funcionar ";
+        this.mensajeDeCayoServer = "[OFF]El servidor dejo de funcionar ";
+    }
+
+    Notify(){
+        let msg = "";
+        const dat = new Date();
+        if (this.status == "ON"){
+              msg = this.mensajeDeRestaablecimiento ; 
+        }else{
+            msg = this.mensajeDeCayoServer;
+        }
+        return  msg + '[ ' + dat.toDateString() + 'a las: ' + dat.getHours() + ':' + dat.getMinutes() + ':' + dat.getSeconds() + ' ]';
+    }
+}
